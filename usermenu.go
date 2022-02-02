b8538746d64d3352a27324384c5f173ede09bb31
Tamal Saha <tamal@appscode.com>
2022-02-01 21:35:11 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- usermenu.go
+++ usermenu.go
@@ -6,13 +6,14 @@ import (
 	"sort"
 	"strings"
 
+	"k8s.io/client-go/discovery"
+
 	"github.com/zeebo/xxh3"
 
 	"github.com/pkg/errors"
 	core "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apiserver/pkg/authentication/user"
 	cu "kmodules.xyz/client-go/client"
 	rsapi "kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
 	"kmodules.xyz/resource-metadata/hub/menuoutlines"
@@ -21,30 +22,37 @@ import (
 )
 
 type UserMenuDriver struct {
-	kc   client.Client
-	ns   string
-	user string
+	kc    client.Client
+	disco discovery.ServerResourcesInterface
+	ns    string
+	user  string
 }
 
-func NewUserMenuDriver(kc client.Client, ns, user string) *UserMenuDriver {
+func NewUserMenuDriver(kc client.Client, disco discovery.ServerResourcesInterface, ns, user string) *UserMenuDriver {
 	return &UserMenuDriver{
-		kc:   kc,
-		ns:   ns,
-		user: user,
+		kc:    kc,
+		disco: disco,
+		ns:    ns,
+		user:  user,
 	}
 }
 
 func configmapName(user, menu string) string {
+	// use ui.appscode.com.menu.$menu.$user
+	return fmt.Sprintf("%s.%s.v1.%s.%d", rsapi.SchemeGroupVersion.Group, rsapi.ResourceMenu, menu, hashUser(user))
+}
+
+func hashUser(user string) uint64 {
 	h := xxh3.New()
 	if _, err := h.WriteString(user); err != nil {
 		panic(errors.Wrapf(err, "failed to hash user %s", user))
 	}
-	return fmt.Sprintf("%s.%s.%s.%d", rsapi.SchemeGroupVersion.Group, rsapi.ResourceMenuOutline, menu, h.Sum64())
+	return h.Sum64()
 }
 
 //nolint
-func getMenuName(user user.Info, cmName string) (string, error) {
-	str := strings.TrimSuffix(cmName, "."+user.GetUID())
+func getMenuName(user string, cmName string) (string, error) {
+	str := strings.TrimSuffix(cmName, fmt.Sprintf(".%d", hashUser(user)))
 	idx := strings.LastIndexByte(str, '.')
 	if idx == -1 {
 		return "", fmt.Errorf("configmap name %s does not match expected menuoutline name format", cmName)
@@ -57,53 +65,58 @@ const (
 	keyUsername = "username"
 )
 
-func extractMenu(cm *core.ConfigMap) (*rsapi.MenuOutline, error) {
+func extractMenu(cm *core.ConfigMap) (*rsapi.Menu, error) {
 	data, ok := cm.Data[keyMenu]
 	if !ok {
 		return nil, apierrors.NewInternalError(fmt.Errorf("ConfigMap %s/%s does not name data[%q]", cm.Namespace, cm.Name, keyMenu))
 	}
-	var obj rsapi.MenuOutline
+	var obj rsapi.Menu
 	if err := yaml.Unmarshal([]byte(data), &obj); err != nil {
 		return nil, err
 	}
 	return &obj, nil
 }
 
-func (r *UserMenuDriver) Get(menu string) (*rsapi.MenuOutline, error) {
+func (r *UserMenuDriver) Get(menu string) (*rsapi.Menu, error) {
 	cmName := configmapName(r.user, menu)
 	var cm core.ConfigMap
 	err := r.kc.Get(context.TODO(), client.ObjectKey{Namespace: r.ns, Name: cmName}, &cm)
 	if apierrors.IsNotFound(err) {
-		return menuoutlines.LoadByName(menu)
+		return RenderAccordionMenu(r.kc, r.disco, menu)
 	} else if err != nil {
 		return nil, err
 	}
 	return extractMenu(&cm)
 }
 
-func (r *UserMenuDriver) List() (*rsapi.MenuOutlineList, error) {
-	allMenus := map[string]rsapi.MenuOutline{}
-	for _, m := range menuoutlines.List() {
-		allMenus[m.Name] = m
-	}
-
+func (r *UserMenuDriver) List() (*rsapi.MenuList, error) {
 	var list core.ConfigMapList
 	err := r.kc.List(context.TODO(), &list, client.InNamespace(r.ns), client.MatchingLabels{
 		"k8s.io/group":     rsapi.SchemeGroupVersion.Group,
-		"k8s.io/kind":      rsapi.ResourceKindMenuOutline,
+		"k8s.io/kind":      rsapi.ResourceKindMenu,
 		"k8s.io/owner-uid": r.user,
 	})
 	if apierrors.IsNotFound(err) {
-		menuoutlines.List()
-		return &rsapi.MenuOutlineList{
+		names := menuoutlines.Names()
+
+		menus := make([]rsapi.Menu, 0, len(names))
+		for _, name := range names {
+			if menu, err := RenderAccordionMenu(r.kc, r.disco, name); err != nil {
+				return nil, err
+			} else {
+				menus = append(menus, *menu)
+			}
+		}
+		return &rsapi.MenuList{
 			TypeMeta: metav1.TypeMeta{},
 			// ListMeta: ,
-			Items: menuoutlines.List(),
+			Items: menus,
 		}, nil
 	} else if err != nil {
 		return nil, err
 	}
 
+	allMenus := map[string]rsapi.Menu{}
 	for _, cm := range list.Items {
 		menu, err := extractMenu(&cm)
 		if err != nil {
@@ -116,24 +129,34 @@ func (r *UserMenuDriver) List() (*rsapi.MenuOutlineList, error) {
 		allMenus[menu.Name] = *menu
 	}
 
-	menus := make([]rsapi.MenuOutline, 0, len(allMenus))
+	for _, name := range menuoutlines.Names() {
+		if _, ok := allMenus[name]; !ok {
+			if menu, err := RenderAccordionMenu(r.kc, r.disco, name); err != nil {
+				return nil, err
+			} else {
+				allMenus[name] = *menu
+			}
+		}
+	}
+
+	menus := make([]rsapi.Menu, 0, len(allMenus))
 	for _, rl := range allMenus {
 		menus = append(menus, rl)
 	}
 	sort.Slice(menus, func(i, j int) bool {
 		return menus[i].Name < menus[j].Name
 	})
-	return &rsapi.MenuOutlineList{
+	return &rsapi.MenuList{
 		TypeMeta: metav1.TypeMeta{},
 		// ListMeta: ,
 		Items: menus,
 	}, nil
 }
 
-func (r *UserMenuDriver) Upsert(menu *rsapi.MenuOutline) (*rsapi.MenuOutline, error) {
+func (r *UserMenuDriver) Upsert(menu *rsapi.Menu) (*rsapi.Menu, error) {
 	data, err := yaml.Marshal(menu)
 	if err != nil {
-		return nil, apierrors.NewInternalError(errors.Wrapf(err, "failed to marshal MenuOutline %s into yaml", menu.Name))
+		return nil, apierrors.NewInternalError(errors.Wrapf(err, "failed to marshal Menu %s into yaml", menu.Name))
 	}
 
 	var cm core.ConfigMap
@@ -147,5 +170,5 @@ func (r *UserMenuDriver) Upsert(menu *rsapi.MenuOutline) (*rsapi.MenuOutline, er
 		}
 		return in
 	})
-	return result.(*rsapi.MenuOutline), err
+	return result.(*rsapi.Menu), err
 }
